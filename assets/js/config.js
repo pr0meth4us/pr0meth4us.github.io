43d74f8dd3cb98dffbc883710a9d6cf56365416e
SOEUNG Phearaneron <2022296soeung@aupp.edu.kh>
2026-07-04 12:33:50 +0700

    style: redesign subpages to raw monochrome brutalist and fix clean URLs

---
+++ assets/js/config.js
@@ -0,0 +1,6 @@
+// Global Configuration
+const CONFIG = {
+    // Set this to true when you are employed and want to show the CRT terminal screen
+    // Set this to false to show your normal full portfolio
+    isEmployed: false
+};
